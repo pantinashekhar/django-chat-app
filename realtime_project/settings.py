169073d88e9e7755545170d1019f8c6a56bfec14
rajupantina91-cloud <raju.pantina91@gmail.com>
2025-12-20 20:30:57 +0530

    added postgresql database

--- realtime_project/settings.py
+++ realtime_project/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'realtime_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
+
 
 
 # Password validation
