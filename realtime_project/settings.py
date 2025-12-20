1b211cc16c0bd61523d724334702e46e6b9720ee
rajupantina91-cloud <raju.pantina91@gmail.com>
2025-12-20 20:45:00 +0530

    settting.py changed

--- realtime_project/settings.py
+++ realtime_project/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#v^p63z=@sa&pt*rer8*%sti$3m14fjfj85kx4xir@_qdjp9!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-chat-app-cfa545f2aeff.herokuapp.com',
+    'localhost',
+    '127.0.0.1',]
 
 
 # Application definition
